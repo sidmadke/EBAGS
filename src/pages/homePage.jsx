a24d328859048713d6ec68a12baa6608beba24cf
rahulchavan3 <rahulchavan3030@gmail.com>
2022-09-27 00:06:15 +0530

    demo commit

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -3,19 +3,17 @@ import { Route, Switch } from 'react-router-dom';
 import Footer from '../components/footer';
 import HomeCards from '../components/home.cards';
 import HomeCarousal from '../components/home.carousal';
-import Navbar from '../components/navbar';
-import About from './about';
-import Contact from './contact';
+
 
 const HomePage = () => {
     return (
         <div>
-            
-            
-            <h1>WoodWorks Shop</h1>
+
+
+            <h1>WoodWorks </h1>
             <HomeCarousal />
             <HomeCards />
-            
+
         </div>
     )
 }
