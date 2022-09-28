1133edff22e8e211cebca9a6057230c10a1e04f6
Asad Majgaonkar <asadmajgaonkar4@gmail.com>
2022-09-28 20:36:14 +0530

    added carousal

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,16 +4,16 @@ const Footer = () => {
     return (
         <div className='footer py-4'>
             <div className='d-flex justify-content-center pb-3'>
-                <a href="#" class="px-3 footer-icon"><i class="fa-brands fa-square-facebook"
+                <a href="#" className="px-3 footer-icon"><i className="fa-brands fa-square-facebook"
                 /></a>
-                <a href="#" class="px-3 footer-icon"><i class="fa-brands fa-twitter"
+                <a href="#" className="px-3 footer-icon"><i className="fa-brands fa-twitter"
                 /></a>
-                <a href="#" class="px-3 footer-icon"><i class="fa-brands fa-youtube"
+                <a href="#" className="px-3 footer-icon"><i className="fa-brands fa-youtube"
                 /></a>
-                <a href="#" class="px-3 footer-icon"><i class="fa-brands fa-instagram"
+                <a href="#" className="px-3 footer-icon"><i className="fa-brands fa-instagram"
                 /></a>
             </div>
-            <div class="text-center">
+            <div className="text-center">
                 <p className='footer-text m-0'>&copy; 2021. Foodies. All Right Reserved</p>
             </div>
         </div>
--- src/components/home.carousal.jsx
+++ src/components/home.carousal.jsx
@@ -1,50 +1,41 @@
 import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-
-
+import couchCover from '../assets/couch.jpg'
+import bedCover from '../assets/bed.jpg'
+import tableCover from '../assets/table.jpg'
+import cupboardCover from '../assets/cupboard.jpg'
+import chairCover from '../assets/chair.jpg'
 
 const HomeCarousal = () => {
-    return (
-        <div >
-             <Carousel>
-      <Carousel.Item interval={1000}>
-        <img
-          className="d-block w-100 "
-          src="https://images.pexels.com/photos/276583/pexels-photo-276583.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={500}>
-        <img
-          className="d-block w-100"
-          src="https://images.pexels.com/photos/1350789/pexels-photo-1350789.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://images.pexels.com/photos/271816/pexels-photo-271816.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+  return (
+    <div >
+      <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
-    );
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={couchCover} class="d-block w-100" alt="..."/>
+          </div>
+          <div class="carousel-item">
+            <img src={bedCover} class="d-block w-100" alt="..."/>
+          </div>
+          <div class="carousel-item">
+            <img src={tableCover} class="d-block w-100" alt="..."/>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default HomeCarousal;
